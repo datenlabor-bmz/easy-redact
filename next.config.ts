33b8e92567a8aa280e1473e39cc047c9de63f14b
Peter Kumberger <peter.kumberger@bmz.bund.de>
2026-07-27 09:41:05 +0200

    Actually read the preferred browser language to set the default language

--- next.config.ts
+++ next.config.ts
@@ -36,9 +36,6 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   serverExternalPackages: ['undici'],
   ...(basePath ? { basePath, assetPrefix: basePath, trailingSlash: true } : {}),
-  async redirects() {
-    return [{ source: '/', destination: '/en', permanent: false }]
-  },
   async headers() {
     return [{ source: '/(.*)', headers: securityHeaders }]
   },
